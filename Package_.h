27c41addbcf55424f562ce20f23cbf5b42232504
Bryan8x8 <bryandelgado99@yahoo.com>
2018-03-05 21:43:08 -0800

    Update Package_.h

--- Package_.h
+++ Package_.h
@@ -1,30 +1,28 @@
-#ifndef PACKAGE_
-#define PACKAGE_
+#ifndef PACKAGER_
+#define PACKAGER_
 #include <iostream>
 #include <vector>
 #include <string>
+#include "Package_.h"
 
 using namespace std;
 
-class Package{ 
-    
-    public:  
-    
-    char getLogic(int);
-    string getCommands(int);
-    vector<char> getLogicVec();
-    vector<string> getCommandVec();
-    void prepareArgVec(vector<string>);
-    void addCommand(string);
-    void addLogic(char);
-    void getVecSizes();
-    void printContents();
-    
-    protected:
-        
-    vector<char> logic;
-    vector<string> commands;
-};
+class Packager {
+    //commandVec contains all the split commands, it has still not been processed to deal with boolean logic.
+    vector <string> commandVec;
+    vector<Package> packageVector;
 
+  public:
+    void commandParse (string);
+    void packageCommands();
+    vector<string> returnVector();
+    vector<Package> returnPackageVector();
+    void displayCmdVector();
+    Package returnPackage(int);
+    bool detectParen(string); //this exists to tell us if there are proper parenthesis within the input
+    string parseParen(string);
+    string parenCmdParse(string);
+    string removeWhiteSpace(string);
+}; 
 
-#endif 
+#endif
